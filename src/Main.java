bd769ed416d8d5956238189bcb2fdb02c01c633b
Gabriel John Levy M. Santiago <gjsantiago42936@liceo.edu.ph>
2025-07-31 15:28:30 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Test Branch");
+package bsit2-3-oop.lab-activity-2;
+
+import java.util.Scanner;
+
+public class NumberTotal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int totalNumber = 0;
+        int number;
+
+        
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            number = scanner.nextInt();
+            totalNumber += number;
+        }
+
+        
+        System.out.println("\nTotal Number: " + totalNumber);
+        
+        scanner.close();
     }
 }
