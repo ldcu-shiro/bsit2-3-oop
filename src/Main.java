2317923771af7b43ba1c0a3dc9270ef4517cb594
Gabriel John Levy M. Santiago <gjsantiago42936@liceo.edu.ph>
2025-07-31 15:57:44 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,37 @@
-package bsit2-3-oop.lab-activity-2;
-
 import java.util.Scanner;
 
 public class NumberTotal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int totalNumber = 0;
-        int number;
+        int totalSum = 0;
+        int numberOfInputs = 5;  
+        int[] numbers = new int[numberOfInputs];
 
-        
-        for (int i = 1; i <= 5; i++) {
-            System.out.print("Enter number " + i + ": ");
-            number = scanner.nextInt();
-            totalNumber += number;
+        System.out.println("Welcome to the Number Total Program!");
+
+
+        for (int i = 0; i < numberOfInputs; i++) {
+            boolean validInput = false;
+
+            while (!validInput) {
+                System.out.print("Enter number " + (i + 1) + ": ");
+                try {
+                    numbers[i] = Integer.parseInt(scanner.nextLine());
+                    totalSum += numbers[i];
+                    validInput = true; // Break out of the loop if input is valid
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid input! Please enter a valid integer.");
+                }
+            }
         }
 
-        
-        System.out.println("\nTotal Number: " + totalNumber);
+
+        System.out.println("\nYou entered the following numbers:");
+        for (int i = 0; i < numberOfInputs; i++) {
+            System.out.println("Number " + (i + 1) + ": " + numbers[i]);
+        }
+
+        System.out.println("\nTotal Sum: " + totalSum);
         
         scanner.close();
     }
