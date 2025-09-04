efeb25b9c2374d66bf0910dd7e4b7f8121d1d2be
Gabriel John Levy M. Santiago <gjsantiago42936@liceo.edu.ph>
2025-09-04 15:52:17 +0800

    Book.java

---
+++ src/Book.java
@@ -0,0 +1,42 @@
+public class Book {
+    private String title;
+    private String author;
+    private int pages;
+    private boolean isAvailable;
+    private double averageRating;
+
+    public Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isAvailable = true;
+        this.averageRating = 0.0;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void displayInfo() {
+        String ratingLevel = getRatingLevel();
+        System.out.println("Book: " + title + " by " + author + ", Average Rating: " + averageRating + ", Level: " + ratingLevel);
+    }
+
+    public void setRating(double rating) {
+        if (rating >= 1 && rating <= 5) {
+            averageRating = rating;
+        } else {
+            System.out.println("Error: Invalid rating: must be 1-5 stars");
+        }
+    }
+
+    private String getRatingLevel() {
+        if (averageRating >= 4.0) {
+            return "Good";
+        } else if (averageRating >= 3.0) {
+            return "Average";
+        } else {
+            return "Poor";
+        }
+    }
+}
